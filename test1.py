5365decb8aa43f228b75eeed3a461a8032c3bf5e
Sreeparno <66938611+god-love-music@users.noreply.github.com>
2022-01-09 22:44:45 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,24 @@
+def local_function():
+    a = 1
+    b = []
+    st="Debjit"
+    for i in range(10):
+        print(st)
+    c = {}
+    d = 4.22
+    for i in range(10):
+        print("Shamik")
+        for j in range(20):
+            print("Debanjan")
+            for k in range(30):
+                print("Sreeparno")
+        for m in range(5):
+            while(m<10):
+                while(i<6):
+                    print("Sukanya")
+jh=4
+local_function()
+kh=10
+
+    
+
---
+++ test4.py
@@ -0,0 +1,9 @@
+def local_function():
+    a = 1
+    b = []
+    st="Debjit"
+    c = {}
+    d = 4.22
+    
+    ss=locals()
+    return ss
